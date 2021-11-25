3fe9bac247faed9b4df41e38f434df3b3a275aa3
Dario0122 <dario.di.cuia@gmail.com>
2021-11-25 16:45:31 +0100

    main

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+      <img src="../assets/logo (1).png" alt="spotify">
+  </header>
+</template>
+
+<script>
+export default {
+name: 'Header',
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/style/variables';
+
+header{
+    background: $primary-color;
+
+    img{
+        width: 60px;
+        padding: 5px
+    }
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,49 @@
+<template>
+  <section>
+      <div class="container ">
+          <div class="card">
+
+          </div>
+      </div>
+
+  </section>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'Main',
+    data(){
+        return{
+            albumsList: [],
+        };
+    },
+    create(){
+        this.getAlbums();
+    },
+    methods:{
+        getAlbums(){
+            axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then(result => { 
+                console.log(result.data);
+                this.albumsList = result.data;
+            })
+            .catch(error => console.log(error.data))
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/style/variables';
+    section{
+        background: $secondary-color;
+        height: 100vh;
+
+        .container{
+            padding-top: 100px;
+        }
+        
+    }
+</style>
